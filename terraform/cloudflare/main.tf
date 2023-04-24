ffeab9e1831a5344994bc54fc44adcf51dd4df4b
Mihir Samdarshi <msamdars@stanford.edu>
2023-04-24 00:09:26 -0700

    chore: update subdomain used for home

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -83,9 +83,9 @@ data "http" "ipv4" {
 }
 
 resource "cloudflare_record" "ipv4" {
-  name    = "ipv4"
+  name    = "local"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = chomp(data.http.ipv4.body)
+  value   = chomp(data.http.ipv4.response_body)
   proxied = true
   type    = "A"
   ttl     = 1
@@ -94,7 +94,7 @@ resource "cloudflare_record" "ipv4" {
 resource "cloudflare_record" "root" {
   name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value   = "local.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
   type    = "CNAME"
   ttl     = 1
